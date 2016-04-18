9ae92834a142dd9204ab1a148abc7987fbf486ef
Frank Kessler <frank-kessler@gmx.de>
2016-04-18 15:31:28 +0200

    finished miwe

--- PhysicalComputing/miwe.c
+++ PhysicalComputing/miwe.c
@@ -1,5 +1,7 @@
 #include <stdio.h> // binde Deklarationen ein (I/O)
 #include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
 
 
 
@@ -7,22 +9,35 @@ int main(int argc, char *argv[]){
 	
 	//Ueberpruefung, ob Argumente uebergeben wurden
 	if (argc <= 1) {
-	printf("Keine Argumente übergeben! \n");
+	printf("No arguments given! \n");
 	} else {
 
-	double  miwe = 0;
+		double  miwe = 0;
 	
-	//summierung der einzelnen Elemnte von argv
-	for(int i=1; i<argc; i++){
-    		miwe += atof(argv[i]);
- 	 }
+		int invalidArgs = 1;
+		//summierung der einzelnen Elemnte von argv
+		for(int i=1; i<argc; i++){
+				//char arg = *argv[i];
+				if (isdigit(*argv[i]) || *argv[i] == '-'){
+					miwe += atof(argv[i]);
+				} else {
+					printf("Only digits are allowed as argument! The non-digit Input will be ignored \n");
+					invalidArgs++;
+				}
+				
+	 	 }
 	
-	//Berechnung des Mittelwertes
-	miwe /= argc-1;
-	
-	//Ausgabe
- 	printf("Der Mittelwert ist: %f \n", miwe);
-
+		//Berechnung des Mittelwertes
+		miwe /= argc-invalidArgs;
+		
+		//Ausgabe
+		if (isnan(miwe)){
+			printf("No valid arguments \n");
+		} else {
+			printf("mean value is: %f \n", miwe);
+		}
+		
 	}
+	
  	return 0; // melde dem Betriebsystem Erfolg
 }
