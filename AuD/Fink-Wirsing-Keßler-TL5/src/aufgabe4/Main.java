d51f53188bd0b30099422441f22890e054a58ee2
Frank Kessler <frank-kessler@gmx.de>
2016-06-28 15:42:51 +0200

    trying stuff

--- AuD/Fink-Wirsing-Keßler-TL5/src/aufgabe4/Main.java
+++ AuD/Fink-Wirsing-Keßler-TL5/src/aufgabe4/Main.java
@@ -10,10 +10,22 @@ public class Main {
 		IGraph iGraph = new GraphImpl();
 		
 		//for debugging
+		
 		List<Node> nodes = iGraph.topSortGraph();
+		
+		while (!nodes.isEmpty()) {
+			print(nodes);
+			
+			nodes.remove(0);
+		}
+	}
+
+	private static void print(List<Node> nodes) {
+		System.out.println("Topologic Sorting: \n");
 		for (Node node : nodes) {
 			System.out.println("Node: " + node + " Succ: " + node.getSuccessors() + " indegree: " + node.getIndegree());
 		}
+		System.out.println();
 	}
 	
 }
