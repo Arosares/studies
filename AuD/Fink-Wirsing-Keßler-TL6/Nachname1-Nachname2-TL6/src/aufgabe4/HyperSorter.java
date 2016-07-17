a521b69519d3a64ddd9314efc988e189ae768d6f
Frank Kessler <frank-kessler@gmx.de>
2016-07-17 15:40:59 +0200

    implemented hyperSort

--- AuD/Fink-Wirsing-Keßler-TL6/Nachname1-Nachname2-TL6/src/aufgabe4/HyperSorter.java
+++ AuD/Fink-Wirsing-Keßler-TL6/Nachname1-Nachname2-TL6/src/aufgabe4/HyperSorter.java
@@ -1,83 +1,118 @@
 package aufgabe4;
 
 public class HyperSorter {
-	
+
 	/**
 	 * The maximum partial array size where quicksort is still applied. When the
 	 * array size drops below this limit, selection sort is used to sort the
 	 * partial array.
 	 */
 	private static final int arraySizeLimit = 5;
-	
+
 	/**
 	 * The starting point of the sorting algorithm.
 	 * 
-	 * @param S The array to be sorted
+	 * @param S
+	 *            The array to be sorted
 	 */
 	public static void sort(int[] S) {
 		recQuickSort(0, S.length - 1, S);
 	}
-	
+
 	/**
 	 * The recursive method to apply quicksort to the whole array or to parts of
 	 * it. When the partial array length drops below the limit given above you
 	 * should use selectionSort.
 	 * 
 	 * @param left
-	 *            The left boundary of the (partial-) array given as an array index.
+	 *            The left boundary of the (partial-) array given as an array
+	 *            index.
 	 * 
 	 * @param right
-	 *            The right boundary of the (partial-) array given as an array index.
-	 *            
+	 *            The right boundary of the (partial-) array given as an array
+	 *            index.
+	 * 
 	 * @param S
-	 * 			  The array to be sorted.
+	 *            The array to be sorted.
 	 */
 	private static void recQuickSort(int left, int right, int[] S) {
 		// TODO implement
-		if (S.length < arraySizeLimit) {
+		if (right - left <= arraySizeLimit) {
 			selectionSort(left, right, S);
 		} else {
-			
+			int partition = partitionIt(left, right, S);
+			recQuickSort(left, partition - 1, S);
+			recQuickSort(partition + 1, right, S);
 		}
 	}
-	
+
 	/**
 	 * The method for finding the element at which you split the (partial-)
 	 * array into two new arrays.
 	 * 
 	 * @param left
-	 *            The left boundary of the (partial-) array given as an array index.
+	 *            The left boundary of the (partial-) array given as an array
+	 *            index.
 	 * 
 	 * @param right
-	 *            The right boundary of the (partial-) array given as an array index.
+	 *            The right boundary of the (partial-) array given as an array
+	 *            index.
 	 * 
 	 * @param S
-	 * 			  The array.
+	 *            The array.
 	 * 
 	 * @return Returns the partition position as an array index.
 	 */
-	private static int partitionIt(int left, int right, int[] S){
+	private static int partitionIt(int left, int right, int[] S) {
 		// TODO implement
-		
-		return Integer.MIN_VALUE;
+		int leftPtr = left - 1;
+		int rightPtr = right;
+		double pivot = S[right];
+
+		while (true) {
+			while (S[++leftPtr] < pivot)
+				; // find bigger item [endet spätestens am Pivot]
+			while (rightPtr > left && S[--rightPtr] > pivot)
+				; // find smaller item
+
+			if (leftPtr >= rightPtr) {
+				break;
+			} else
+				swap(leftPtr, rightPtr, S);
+		}
+		swap(leftPtr, right, S); // store pivot in correct position
+
+		return leftPtr;
 	}
-	
+
 	/**
 	 * A method that represents the selection sort algorithm.
 	 * 
 	 * @param left
-	 *            The left boundary of the (partial-) array given as an array index.
+	 *            The left boundary of the (partial-) array given as an array
+	 *            index.
 	 * 
 	 * @param right
-	 *            The right boundary of the (partial-) array given as an array index.
+	 *            The right boundary of the (partial-) array given as an array
+	 *            index.
 	 * 
 	 * @param S
-	 * 			  The array.
+	 *            The array.
 	 */
 	private static void selectionSort(int left, int right, int[] S) {
 		// TODO implement
+		int minIndex;
+		for (int i = left; i <= right; i++) {
+			minIndex = i;
+			for (int j = i + 1; j <= right; j++) { // search minimum
+				if (S[j] < S[minIndex]) {
+					minIndex = j;
+				}
+			}
+			swap(i, minIndex, S);
+		}
 	}
-	
+
 	/**
 	 * A helper method to swap two values in an array.
 	 */
--- AuD/Fink-Wirsing-Keßler-TL6/Nachname1-Nachname2-TL6/src/aufgabe4/Main.java
+++ AuD/Fink-Wirsing-Keßler-TL6/Nachname1-Nachname2-TL6/src/aufgabe4/Main.java
@@ -8,25 +8,24 @@ public class Main {
 	 * Generates a random integer array (you don't say).
 	 * 
 	 * @param size
-	 * 			The size of the generated array
-	 * @return
-	 * 			The random array
+	 *            The size of the generated array
+	 * @return The random array
 	 */
 	private static int[] generateRandomArray(int size) {
-        int[] output = new int[size];
-        Random random = new Random(1337);
-        for (int i = 0; i < size; i++) {
-            output[i] = random.nextInt(size * 10);
-        }
-        
-        return output;
-    }
+		int[] output = new int[size];
+		Random random = new Random(1337);
+		for (int i = 0; i < size; i++) {
+			output[i] = random.nextInt(size * 10);
+		}
+
+		return output;
+	}
 
 	/**
 	 * Nicely prints out an array. Use for testing.
 	 * 
 	 * @param array
-	 * 			The array to print out
+	 *            The array to print out
 	 */
 	private static void printArray(int array[]) {
 		System.out.print("[");
@@ -40,6 +39,25 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		// TODO test your implementations. For measuring the time you can use System.currentTimeMillis()
+		//BottomUpHeapsort
+//		int[] arr = generateRandomArray(10);
+//		System.out.println("Before BottomUp");
+//		printArray(arr);
+//		System.out.println("After BottomUp");
+//		BottomUpHeapsort.sort(arr);
+//		printArray(arr);
+		
+		//HyperSort
+		
+		int[] hyperSortArray = generateRandomArray(1000000);
+		//disabled printing of the array for very big array because it takes forever
+		//and the text overlaps on my eclipse-install
+		//		printArray(hyperSortArray);
+		long startTime = System.nanoTime();
+		HyperSorter.sort(hyperSortArray);
+		long endTime = System.nanoTime();
+//		printArray(hyperSortArray);
+		long totalTime = (endTime - startTime) / 1000000; //divide by 1000000 (from nanoseconds to milliseconds)
+		System.out.println("ms: "+ totalTime);
 	}
 }
